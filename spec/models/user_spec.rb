87c7f846d6f2586930996861cff5e6efa63e8924
Muyiwa Olu-Ogunleye <m.oluogunleye94@gmail.com>
2016-10-19 17:29:22 +0100

    Updated user spec to use updated user fields

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "can create a new user" do
-    user = User.create(name: 'John', email: 'test@johndoe.com')
+    user = User.create(name: 'John', username: 'JohnDoe', image: 'link-to-image')
     expect(user.name).to eq 'John'
+    expect(user.username).to eq 'JohnDoe'
+    expect(user.image).to eq 'link-to-image'
   end
 end
